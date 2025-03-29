6d41b05925b91bd6b6071ffecc33dcc15f394e95
Daniel1991T <dantifui.91@gmail.com>
2025-03-29 21:07:04 +0200

    feat: add Inter and Grotesk fonts with full setup and integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="flex-center flex w-20 rounded-2 bg-slate-400">
-        Hello World!
-      </h1>
+      <h1 className="h1-bold">Hello World!</h1>
+      <h1 className="h1-bold font-space-grotesk">Hello World!</h1>
     </main>
   );
 }
