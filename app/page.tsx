9f7a2683bbb2dca03eaaf44f764765eca84d0217
Daniel1991T <dantifui.91@gmail.com>
2025-03-29 20:52:15 +0200

    setup global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="flex w-20 bg-slate-400">Hello World!</h1>
+      <h1 className="flex-center flex w-20 rounded-2 bg-slate-400">
+        Hello World!
+      </h1>
     </main>
   );
 }
