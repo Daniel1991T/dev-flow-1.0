88d7cc83ff98b57ab7cfbfc148501a6daf45997b
Daniel1991T <dantifui.91@gmail.com>
2025-03-31 23:55:56 +0300

    feat: implement OAuth login with Google and GitHub

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <main>
       <h1 className="h1-bold">Hello World!</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({
+            redirectTo: ROUTES.SIGN_IN,
+          });
+        }}
+      >
+        <Button type="submit">Logout</Button>
+      </form>
     </main>
   );
-}
+};
+
+export default Home;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,10 +1,34 @@
+"use client";
 import clsx from "clsx";
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 import React from "react";
+
+import ROUTES from "@/constants/routes";
+import { toast } from "@/hooks/use-toast";
 
 import { Button } from "../ui/button";
 
 const SocialAuthForm = () => {
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Sign-in Failed",
+        description:
+          error instanceof Error
+            ? error.message
+            : "An error occurred during sign-in.",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
       <SocialButton
@@ -12,11 +36,15 @@ const SocialAuthForm = () => {
         altImg="Github Logo"
         title="Log in with Github"
         imageClassName="invert-colors"
+        handleSignIn={handleSignIn}
+        provider="github"
       />
       <SocialButton
         srcImg="/icons/google.svg"
         altImg="Google Logo"
         title="Log in with Google"
+        handleSignIn={handleSignIn}
+        provider="google"
       />
     </div>
   );
@@ -27,6 +55,8 @@ type SocialButtonProps = {
   altImg: string;
   imageClassName?: string;
   title: string;
+  handleSignIn: (provider: "github" | "google") => void;
+  provider: "google" | "github";
 };
 
 const SocialButton = ({
@@ -34,9 +64,14 @@ const SocialButton = ({
   altImg,
   imageClassName,
   title,
+  handleSignIn,
+  provider,
 }: SocialButtonProps) => {
   return (
-    <Button className="background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5">
+    <Button
+      className="background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5"
+      onClick={() => handleSignIn(provider)}
+    >
       <Image
         src={srcImg}
         alt={altImg}
