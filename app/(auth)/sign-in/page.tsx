dd386020a30e05ebaaf4f31947d7254d897d1b21
Daniel1991T <dantifui.91@gmail.com>
2025-04-01 21:15:10 +0300

    feat: create UI for sign-in and sign-up pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,26 @@
+"use client";
 import React from "react";
 
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      onSubmit={(data) =>
+        Promise.resolve({
+          success: true,
+          data,
+        })
+      }
+      defaultValues={{
+        email: "",
+        password: "",
+      }}
+    />
+  );
 };
 
 export default SignIn;
