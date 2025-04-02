e91237869c9754dcc185f1451fa3b0e784c2c48f
Daniel1991T <dantifui.91@gmail.com>
2025-04-02 21:06:02 +0300

    feat: establish structure for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,30 @@
-import { auth } from "@/auth";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
-
   return (
-    <main>
-      <h1 className="h1-bold">Hello World!</h1>
-    </main>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+        <p>Question Card 5</p>
+      </div>
+    </>
   );
 };
 
