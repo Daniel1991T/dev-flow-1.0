66dc9cd2e93f4a3c09bf40d1371d6a73e4ffdd11
Daniel1991T <dantifui.91@gmail.com>
2025-04-04 22:55:31 +0300

    feat: implement Tag Input UI and logic

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -12,23 +13,56 @@ type TagCardProps = {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 };
 
-const TagCard = ({ _id, name, questions, showCount }: TagCardProps) => {
+const TagCard = ({
+  _id,
+  name,
+  questions,
+  showCount,
+  compact,
+  remove,
+  isButton,
+  handleRemove,
+}: TagCardProps) => {
   const iconClassName = getDeviconClassName(name);
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+  const Content = () => (
+    <>
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-wrap gap-2 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClassName} text-sm`}></i>
           <span>{name}</span>
         </div>
+        {remove ? (
+          <Image
+            src="/icons/close.svg"
+            alt="close icon"
+            width={12}
+            height={12}
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        ) : null}
       </Badge>
       {showCount && (
         <p className="small-medium text-dark500_light700">{questions}</p>
       )}
-    </Link>
+    </>
   );
+  if (compact) {
+    return isButton ? (
+      <button className="flex justify-between gap-2" type="button">
+        <Content />
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        <Content />
+      </Link>
+    );
+  }
 };
 
 export default TagCard;
--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -49,7 +49,7 @@ const Editor = ({ fieldChange, editorRef, ...props }: EditorProps) => {
     <MDXEditor
       key={resolvedTheme}
       onChange={fieldChange}
-      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border"
       plugins={[
         // Example Plugin Usage
         headingsPlugin(),
