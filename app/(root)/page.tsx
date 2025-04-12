31ed691abd3cdb71d2785abeea4a09b95255269f
Daniel1991T <dantifui.91@gmail.com>
2025-04-12 18:00:09 +0300

    feat: enhance TagPage with question fetching, rendering, and search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -38,7 +38,7 @@ const Home = async ({ searchParams }: SearchParams) => {
       </section>
       <section className="mt-11">
         <LocalSearch
-          route="/"
+          route={ROUTES.HOME}
           imgSrc="/icons/search.svg"
           placeholder="Search questions..."
           otherClasses="flex-1"
--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,10 +1,54 @@
 import React from "react";
 
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { UI_STATES } from "@/constants/states";
+import { GetTagQuestions } from "@/lib/actions/tag.actions";
 import { RouteParams } from "@/types/global";
 
-const TagPage = async ({ params }: RouteParams) => {
+const TagPage = async ({ params, searchParams }: RouteParams) => {
   const { id } = await params;
-  return <div>{id}</div>;
-};
+  const { query, page, pageSize } = await searchParams;
 
+  const { success, data, error } = await GetTagQuestions({
+    tagId: id,
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+  });
+
+  const { tag, questions } = data || {};
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900 capitalize">
+          {tag?.name}
+        </h1>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAG(id)}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <DataRenderer
+        success={success}
+        error={error}
+        data={questions}
+        empty={UI_STATES.EMPTY_QUESTION}
+        render={(questions) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {questions.map((question) => (
+              <QuestionCard key={question._id} question={question} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
+};
 export default TagPage;
